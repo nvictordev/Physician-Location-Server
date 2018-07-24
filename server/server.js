ea85bd268d1ae34a2da67947aaccba6011000f76
Victor Nhem <nhemvictor@gmail.com>
2018-07-24 16:44:04 -0500

    Implement correct PORT & Node engine version

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {mongoose} = require('./db/mongoose');
 const {Contact} = require('./models/contact');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5001;
 app.use(cors())
 app.use(bodyParser.json());
 
@@ -80,8 +80,9 @@ app.get('/address', (req, res) => {
 
 app.use((req, res, next) => {
   res.status(404).send('<h2 align=center> Page Not Found!</h2>');
+  next();
 });
 
-app.listen(5001, () => {
+app.listen(port, () => {
   console.log(`Started on port ${port}`)
 });
