2bebe87dae72e27750b351497521753398870356
Victor Nhem <nhemvictor@gmail.com>
2018-07-25 17:03:29 -0500

    Fix collection name typo

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const {Contact} = require('./models/contact');
+const {contacts} = require('./models/contact');
 
 const app = express();
 const port = process.env.PORT || 5001;
