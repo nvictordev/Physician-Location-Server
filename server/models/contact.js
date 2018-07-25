5ec28e7c150d9630135165409a6f9f672456c648
Victor Nhem <nhemvictor@gmail.com>
2018-07-25 17:23:56 -0500

    Implement mongoose schema

--- server/models/contact.js
+++ server/models/contact.js
@@ -1,24 +1,28 @@
 const mongoose = require('mongoose');
 
-const contacts = mongoose.model('contacts', {
-  Physician_First_Name: {
-    type: String,
-    required: true,
-    minlength: 1
-  },
-  Physician_Middle_Name: {
-    type: String,
-    minlength: 1 
-  },
-  Physician_Last_Name: {
-    type: String,
-    required: true,
-    minlength: 1
-  },
-  Recipient_Primary_Business_Street_Address_Line1: {},
-  Recipient_City: {},
-  Recipient_State: {},
-  Recipient_Zip_Code: {}
-});
+mongoose.connection.once('open', function callback() {
+  let contactsSchema = mongoose.model({
+    Physician_First_Name: {
+      type: String,
+      required: true,
+      minlength: 1
+    },
+    Physician_Middle_Name: {
+      type: String,
+      minlength: 1 
+    },
+    Physician_Last_Name: {
+      type: String,
+      required: true,
+      minlength: 1
+    },
+    Recipient_Primary_Business_Street_Address_Line1: {},
+    Recipient_City: {},
+    Recipient_State: {},
+    Recipient_Zip_Code: {}
+  });
+  let contacts = mongoose.model('contacts', contactsSchema)
+  return mongoose.connection.close();
+})
 
 module.exports = {contacts};
