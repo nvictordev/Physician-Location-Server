d0f32cc035fc3677f3f51b4f18069f0844025208
Victor Nhem <nhemvictor@gmail.com>
2018-07-25 19:10:19 -0500

    Changed to contacts collection

--- server/models/contact.js
+++ server/models/contact.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://victordev:victordev7@ds147361.mlab.com:47361/heroku_n26zxzb5');
 
-const Contact = mongoose.model('Contact', {
+const Contact = mongoose.model('contacts', {
   Physician_First_Name: {
     type: String,
     required: true,
@@ -23,7 +23,5 @@ const Contact = mongoose.model('Contact', {
   Recipient_State: {},
   Recipient_Zip_Code: {}
 });
-
-module.exports = {Contact};
 
 module.exports = {Contact};
